d6bb98f6869b9d0cbdb5e24235531b46f7324807
Felipe <felipewill2@gmail.com>
2016-09-28 20:37:00 -0300

    testing build webpack heroku

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
-var open = require('open');
+const path = require('path')
+const express = require('express')
 
-new WebpackDevServer(webpack(config), config.devServer)
-  .listen(config.port, 'localhost', function (err) {
-    if (err) {
-      console.log(err);
-    }
-    console.log('Listening at localhost:' + config.port);
-    console.log('Opening your system browser...');
-    open('http://localhost:' + config.port + '/webpack-dev-server/');
-  });
+const port = (process.env.PORT || 8000);
+const app = express();
+const indexPath = path.join(__dirname, './index.html');
+const publicPath = express.static(path.join(__dirname, '../dist'));
+
+app.use('/dist', publicPath)
+app.get('/', function (_, res) { res.sendFile(indexPath) })
+
+if (process.env.NODE_ENV !== 'production') {
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackHotMiddleware = require('webpack-hot-middleware');
+  const config = require('./webpack.dev.config.js');
+  const compiler = webpack(config);
+
+  app.use(webpackHotMiddleware(compiler));
+  app.use(webpackDevMiddleware(compiler, {
+    noInfo: true,
+    publicPath: config.output.publicPath
+  }));
+}
+
+app.listen(port);
+console.log(`Listening at http://localhost:${port}`);
