ae52f61261b0fd64f1ec75878b55cf56937ceee8
Felipe <felipewill2@gmail.com>
2016-09-28 20:48:02 -0300

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express')
 const port = (process.env.PORT || 8000);
 const app = express();
 const indexPath = path.join(__dirname, './index.html');
-const publicPath = express.static(path.join(__dirname, '../dist'));
+const publicPath = express.static(path.join(__dirname, './dist'));
 
 app.use('/dist', publicPath)
 app.get('/', function (_, res) { res.sendFile(indexPath) })
